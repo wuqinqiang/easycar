5a780f0e03586ff9c636bbf3bda86edec255c662
吴亲强 <wuqq@weipaitang.com>
2021-11-01 22:24:28 +0800

    db

---
+++ gen_model.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+)
+
+// generate code
+func main() {
+	// specify the output directory (default: "./query")
+	// ### if you want to query without context constrain, set mode gen.WithoutContext ###
+
+	outPath := "./internal/query"
+
+	g := gen.NewGenerator(gen.Config{
+		OutPath: outPath,
+		OutFile: outPath + "/query.go",
+		/* Mode: gen.WithoutContext|gen.WithDefaultQuery*/
+		//if you want the nullable field generation property to be pointer type, set FieldNullable true
+		/* FieldNullable: true,*/
+		//If you need to generate index tags from the database, set FieldWithIndexTag true
+		/* FieldWithIndexTag: true,*/
+	})
+
+	// reuse the database connection in Project or create a connection here
+	// if you want to use GenerateModel/GenerateModelAs, UseDB is necessray or it will panic
+	db, _ := gorm.Open(mysql.Open("root:Passw0rd@(127.0.0.1:3306)/easycar?charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(db)
+
+	g.ApplyBasic(
+		g.GenerateModelAs("branch", "Branch"),
+		g.GenerateModelAs("global", "Global"),
+	)
+	g.Execute()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
 module github.com/wuqinqiang/easycar
 
 go 1.17
+
+require (
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/gen v0.1.14
+	gorm.io/gorm v1.21.16
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gorm.io/datatypes v1.0.2 // indirect
+	gorm.io/hints v0.0.0-20210809061251-a20b3c9afa2b // indirect
+)
