08acae34b54d97ff6531659419f1f77c39680985
吴亲强 <wuqq@weipaitang.com>
2021-11-25 23:57:06 +0800

    init client

--- pkg/client/rm.go
+++ pkg/client/rm.go
@@ -10,6 +10,8 @@ import (
 type RM struct {
 	// server Address
 	serverAddress string
+	// the transaction global id
+	gId string
 	// todo some timeout config
 }
 
@@ -18,8 +20,8 @@ type TransactionInterface interface {
 	GetProtocol() string
 }
 
-func NewRM(serverAddress string) *RM {
-	return &RM{serverAddress: serverAddress}
+func NewRM(tmServer string) *RM {
+	return &RM{serverAddress: tmServer}
 }
 
 // Start start for a transaction
@@ -32,15 +34,55 @@ func (r *RM) Start(tran TransactionInterface) (gId string, err error) {
 	req.SetProtocol(tran.GetProtocol())
 
 	resp := new(common.RespGlobalData)
-	err = common.RestyClient.PostJson(r.serverAddress, req, resp)
+	err = common.RestyClient.PostJson(r.serverAddress+"/begin", req, resp)
 	if err != nil {
 		// todo common err handler
 		return
 	}
-	return resp.GetGId(), nil
+	r.gId = resp.GetGId()
+	return r.gId, nil
 }
 
 // RegisterBranch Register transaction branchList to server
-func (r *RM) RegisterBranch(ctx context.Context, branchList []common.BranchData) {
+func (r *RM) RegisterBranch(ctx context.Context, branchList []common.BranchData) error {
+	if len(branchList) == 0 {
+		return fmt.Errorf("")
+	}
+	var (
+		resp common.RespBase
+	)
+	err := common.RestyClient.
+		PostJson(r.serverAddress+"/registerBranch", branchList, resp)
+	if err != nil {
+		// todo handler err for common
+		return fmt.Errorf("er")
+	}
+	return nil
+}
 
+// Commit commit the transaction when all branch success
+func (r *RM) Commit(ctx context.Context) error {
+	var (
+		req  common.ReqCommitData
+		resp common.RespBase
+	)
+	req.SetGId(r.gId)
+	err := common.RestyClient.PostJson(r.serverAddress+"/commit", req, resp)
+	if err != nil {
+
+	}
+	return nil
+}
+
+// Fail fail when any transaction to err
+func (r *RM) Fail(ctx context.Context) error {
+	var (
+		req  common.ReqFailData
+		resp common.RespBase
+	)
+	req.SetGId(r.gId)
+	err := common.RestyClient.PostJson(r.serverAddress+"/fail", req, resp)
+	if err != nil {
+	}
+	return nil
 }
--- pkg/client/tcc.go
+++ pkg/client/tcc.go
@@ -1,6 +1,10 @@
 package client
 
-import "github.com/wuqinqiang/easycar/pkg/common"
+import (
+	"context"
+
+	"github.com/wuqinqiang/easycar/pkg/common"
+)
 
 var _ TransactionInterface = &TCC{}
 
@@ -10,7 +14,10 @@ var (
 	DefaultProtoCol = "http"
 )
 
+type RMFunc func(rmFunc *RM) ([]common.BranchData, error)
+
 type TCC struct {
+	RM
 	protoCol string
 }
 
@@ -22,14 +29,38 @@ func NewTCC(options ...TCCOption) *TCC {
 	return tcc
 }
 
-func (t *TCC) GetTransactionName() common.TransactionName {
+func (tcc *TCC) GetTransactionName() common.TransactionName {
 	return "tcc"
 }
 
-func (t *TCC) GetProtocol() string {
-	return t.protoCol
+func (tcc *TCC) GetProtocol() string {
+	return tcc.protoCol
 }
 
-func (t *TCC) RegisterBranch() {
-
+func (tcc *TCC) RegisterBranch(ctx context.Context) {
+	tcc.RM.RegisterBranch(ctx, nil)
 }
+
+// WeGo begin a transaction for tcc
+func (tcc *TCC) WeGo(ctx context.Context, serverAddress string, rmFunc RMFunc) error {
+	rm := NewRM(serverAddress)
+	gId, err := rm.Start(tcc)
+	if err != nil {
+		// todo log
+		return err
+	}
+	// step1:register branch
+	// step2: call branch try url
+	branchList, err := rmFunc(rm)
+	if err != nil {
+		// todo log
+		return err
+	}
+	rm.RegisterBranch(ctx, branchList)
+
+	return nil
+}
+
+// ask myself some questions
+// 1.how to easy to use?
+//
