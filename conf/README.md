673656c92f045b7ff8e794ab80ba5ad9bbe43c21
吴亲强 <wuqq@weipaitang.com>
2021-12-01 20:40:38 +0800

    init conf

---
+++ conf/README.md
@@ -0,0 +1,1 @@
+### the conf borrowed from the kratos project
--- conf/option.go
+++ conf/option.go
@@ -1,15 +1,47 @@
 package conf
 
+import (
+	"fmt"
+
+	"github.com/wuqinqiang/easycar/conf/encoding"
+)
+
 type Decoder func(value *KeyValue, res map[string]interface{}) error
 
 type OptionFunc func(*options)
 
+var (
+	NotFoundDecoder = fmt.Errorf("not found decoder")
+)
+
 type options struct {
 	decoder Decoder
+	source  Source
 }
 
 func WithDecoder(decoder Decoder) OptionFunc {
 	return func(o *options) {
 		o.decoder = decoder
 	}
+}
+
+func WithSource(source Source) OptionFunc {
+	return func(o *options) {
+		o.source = source
+	}
+}
+
+func DefaultDecoder(res *KeyValue, target map[string]interface{}) error {
+	if res.Format == "" {
+		return NotFoundDecoder
+	}
+	codec, ok := encoding.GetCodecByFormat(res.Format)
+	if !ok {
+		return NotFoundDecoder
+	}
+	err := codec.UnMarshal(res.Value, target)
+	if err != nil {
+		return err
+	}
+	return nil
 }
