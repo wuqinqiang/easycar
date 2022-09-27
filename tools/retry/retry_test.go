d67ac7835d8d936b96fbc16b75a468dd6d45d7c3
wuqinqiang <1185079673@qq.com>
2022-09-27 15:47:55 +0800

    feat:add some  retry test

--- tools/retry/retry_test.go
+++ tools/retry/retry_test.go
@@ -1,6 +1,7 @@
 package retry
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -8,11 +9,11 @@ import (
 )
 
 func TestNewRetry(t *testing.T) {
-	retry := New(3, WithMaxBackOffTime(1*time.Second))
+	retry := New(3, WithMaxBackOffTime(1*time.Second), WithFactor(3))
 
 	assert.Equal(t, time.Second, retry.Duration())
 	assert.Equal(t, uint32(3), retry.allowAttempt)
-	assert.Equal(t, uint32(2), retry.factor)
+	assert.Equal(t, uint32(3), retry.factor)
 	assert.Equal(t, uint32(0), retry.currentAttempt)
 	err := retry.Run(func() error {
 		return nil
@@ -20,4 +21,26 @@ func TestNewRetry(t *testing.T) {
 	assert.Equal(t, nil, err)
 	assert.Equal(t, time.Second, retry.Duration())
 	assert.Equal(t, uint32(1), retry.currentAttempt)
+}
+
+func TestRetry_Run(t *testing.T) {
+	retry := New(3, WithMaxBackOffTime(1*time.Second))
+	err := retry.Run(func() error {
+		return fmt.Errorf("fn err")
+	})
+	assert.Equal(t, err.Error(), "fn err")
+
+	expectedVal := 0
+	retry = New(3, WithMaxBackOffTime(100*time.Millisecond))
+	err = retry.Run(func() error {
+		expectedVal++
+		if expectedVal < 3 {
+			return fmt.Errorf("condition not met")
+		}
+		return nil
+	})
+	assert.Equal(t, err, nil)
+	assert.Equal(t, uint32(3), retry.currentAttempt)
+	assert.Equal(t, 100*time.Millisecond, retry.maxBackOffTime)
+
 }
