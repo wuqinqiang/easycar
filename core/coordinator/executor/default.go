1250cb012b74948f73ab437ea89857508c45832e
wuqinqiang <1185079673@qq.com>
2022-11-11 10:00:33 +0800

    fix: issue 43

--- core/coordinator/executor/default.go
+++ core/coordinator/executor/default.go
@@ -1,9 +1,14 @@
 package executor
 
 import (
+	"time"
+
 	"github.com/wuqinqiang/easycar/core/transport"
 )
 
+// DefaultExecutor executor
 var DefaultExecutor = &executor{
 	manager: transport.NewManager(),
+	// default timeout for branches
+	timeout: 8 * time.Second,
 }
