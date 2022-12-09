f27482838b33aa9c6d1404fd6a1cadbfb02f9dbd
wuqinqiang <1185079673@qq.com>
2022-12-09 18:24:33 +0800

    feat: Add group manager

---
+++ client/group_manger.go
@@ -0,0 +1,31 @@
+package client
+
+import "github.com/wuqinqiang/easycar/core/consts"
+
+type Manger struct {
+	groups []*Group
+	// level current level
+	level consts.Level
+}
+
+func NewManger() *Manger {
+	return &Manger{
+		// default level
+		level: 1,
+	}
+}
+
+func (m *Manger) AddGroup(skip bool, groups ...*Group) *Manger {
+	if skip {
+		m.level++
+	}
+	for _, group := range groups {
+		group.SetLevel(m.level)
+		m.groups = append(m.groups, group)
+	}
+	return m
+}
+
+func (m *Manger) Groups() []*Group {
+	return m.groups
+}
